39842d4b54fafb231abedbc615dac718365e0792
Tawhid <gem@tawhid.fios-router.home>
2020-11-23 22:22:44 -0500

    ball reflects off paddle

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ let ballY = 75;
 let ballSpeedX = 5;
 let ballSpeedY = 7;
 
+const PADDLE_WIDTH = 100;
+const PADDLE_THICKNESS = 10;
+const PADDLE_DIST_FROM_EDGE = 60;
+var paddleX = 400;
+
 let canvas, canvasContext;
+
+function updateMousePos(evt) {
+    var rect = canvas.getBoundingClientRect();
+    var root = document.documentElement;
+
+    var mouseX = evt.clientX - rect.left - root.scrollLeft;
+    // var mouseY = evt.clientY - rect.Top - root.scrollTop;
+
+    paddleX = mouseX - PADDLE_WIDTH/2;
+}
+
 
 window.onload = function() {
     canvas = document.getElementById('gameCanvas');
@@ -12,7 +28,7 @@ window.onload = function() {
     let framesPerSecond = 30;
     setInterval(updateAll, 1000/framesPerSecond);
 
-
+    canvas.addEventListener('mousemove', updateMousePos);
 };
 
 function updateAll() {
@@ -20,26 +36,47 @@ function updateAll() {
     drawAll()
 }
 
+function ballReset() {
+    ballX = canvas.width/2;
+    ballY = canvas.height/2;
+}
+
 function moveBall() {
     // Ball Y-axis
     ballY += ballSpeedY;
 
-    if(ballY < 0){
+    if(ballY < 0){ //top
         ballSpeedY *= -1;
     }
-    if(ballY > canvas.height){
-        ballSpeedY *= -1;
+    if(ballY > canvas.height){ //bottom
+        // ballSpeedY *= -1;
+        ballReset();
     }
 
     // Ball X-axis
     ballX += ballSpeedX;
 
-    if(ballX < 0){
+    if(ballX < 0){ //left
         ballSpeedX *= -1;
     }
-    if(ballX > canvas.width){
+    if(ballX > canvas.width){ //right
         ballSpeedX *= -1;
     }
+
+    var paddleTopEdgeY = canvas.height - PADDLE_DIST_FROM_EDGE;
+    var paddleBottomEdgeY = paddleTopEdgeY + PADDLE_THICKNESS;
+    var paddleLeftEdgeX = paddleX;
+    var paddleRightEdgeX = paddleLeftEdgeX + PADDLE_WIDTH;
+
+    if(
+        ballY > paddleTopEdgeY && //below the top of paddle
+        ballY < paddleBottomEdgeY && //above bottom of paddle
+        ballX > paddleLeftEdgeX && //right of the left side of paddle
+        ballX < paddleRightEdgeX //right of the left side of paddle
+    ) {
+        ballSpeedY *= -1;
+    }
+
 }
 
 
@@ -47,6 +84,9 @@ function drawAll(){
     colorRect(0,0, canvas.width,canvas.height, 'black');
 
     colorCircle(ballX,ballY, 10, 'red');
+
+    colorRect(paddleX,canvas.height - PADDLE_DIST_FROM_EDGE, 
+                PADDLE_WIDTH,PADDLE_THICKNESS, 'white');
 }
 
 function colorRect(topLeftX,topLeftY, boxWidth,boxHeight, fillColor){
